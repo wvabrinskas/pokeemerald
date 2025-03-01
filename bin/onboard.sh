9f391c41adb5df7df214091117327cd374c8134c
William Vabrinskas <wvabrinskas@gmail.com>
2025-03-01 16:16:24 -0500

    update

--- bin/onboard.sh
+++ bin/onboard.sh
@@ -3,6 +3,9 @@
 
 #git clone https://github.com/pret/agbcc
 
+sudo apt update && sudo apt upgrade
+sudo apt install build-essential binutils-arm-none-eabi git libpng-dev
+
 cd agbcc
 ./build.sh
 ./install.sh ../
